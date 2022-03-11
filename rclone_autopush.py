e903346d7144ce0f19ffab539c4be5dfdb6802ea
brylind <75110520+brylind@users.noreply.github.com>
2022-03-11 13:37:36 -0600

    added rclone auto push .py

--- rclone_autopush.py
+++ rclone_autopush.py
@@ -8,14 +8,16 @@ gps_data_path = f"/home/pi/glinda_main/dataFiles/gps"
 mic_remote_loc = f"{hname}/{hname}_mic_data"
 gps_remote_loc = f"{hname}/{hname}_gps_data"
 
+
 try:
-    comline_mic = f'rclone move "{mic_data_path}" "glinda_data:{mic_remote_loc}" --min-age 2m'
-    os.system(comline_mic)
+    while True:
+        comline_mic = f'rclone move "{mic_data_path}" "glinda_data:{mic_remote_loc}" --min-age 2m'
+        os.system(comline_mic)
 
-    comline_mic = f'rclone move "{gps_data_path}" "glinda_data:{gps_remote_loc}" --min-age 2m'
-    os.system(comline_mic)
+        comline_mic = f'rclone move "{gps_data_path}" "glinda_data:{gps_remote_loc}" --min-age 2m'
+        os.system(comline_mic)
 
-    sleep(300)
+        sleep(600)
 except:
     os.system("rclone push attempt failed")
 
