0db7de60490734fc391cf2d55daab262e708333c
brylind <75110520+brylind@users.noreply.github.com>
2022-03-11 13:31:30 -0600

    gemboard.py

--- rclone_autopush.py
+++ rclone_autopush.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from time import sleep
 
 hname = socket.gethostname()
 mic_data_path = f"/home/pi/glinda_main/dataFiles/mic"
@@ -11,7 +12,10 @@ try:
     comline_mic = f'rclone move "{mic_data_path}" "glinda_data:{mic_remote_loc}" --min-age 2m'
     os.system(comline_mic)
 
+    comline_mic = f'rclone move "{gps_data_path}" "glinda_data:{gps_remote_loc}" --min-age 2m'
+    os.system(comline_mic)
 
+    sleep(300)
 except:
     os.system("rclone push attempt failed")
 
