2a0a75815ffd6a42fb32c18fcda8458d65e855ca
brylind <75110520+brylind@users.noreply.github.com>
2022-03-14 14:48:42 -0500

    added testing stuff

--- testing_misc_archived/serial_read.py
+++ testing_misc_archived/serial_read.py
@@ -3,7 +3,7 @@ import time
 import serial
 
 ser = serial.Serial(
-        port='/dev/USB0',
+        port='/dev/ttyS0',
         baudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
