2db8dd6e73fe0148aa3ddf1f0a23d2160f068b6c
brylind <75110520+brylind@users.noreply.github.com>
2022-03-15 07:31:26 -0500

    small changes to rclone

--- rclone_autopush.py
+++ rclone_autopush.py
@@ -14,7 +14,7 @@ try:
         comline_mic = f'rclone move "{mic_data_path}" "glinda_data:{mic_remote_loc}" --min-age 2m'
         os.system(comline_mic)
         os.system("echo mic data pushed to remote drive")
-        comline_gps = f'rclone move "{gps_data_path}" "glinda_data:{gps_remote_loc}" --min-age 20m'
+        comline_gps = f'rclone move "{gps_data_path}" "glinda_data:{gps_remote_loc}" --min-age 2m'
         os.system(comline_gps)
         os.system("echo gps data pushed to remote drive")
         sleep(300)
