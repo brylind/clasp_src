770ff74f8d313f803a5f851f15750d7ea5ec5fcf
brylind <75110520+brylind@users.noreply.github.com>
2022-03-27 15:41:18 -0500

    testing gps time stuff

--- testing_misc_archived/testing_GPS.py
+++ testing_misc_archived/testing_GPS.py
@@ -39,7 +39,7 @@ def GPS():
                     else:
                         dat.append([time(), 0, 0, 0, -1, -1, 0])
                     sleep(1)
-                    print([time(), gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])  # used this for testing - BL
+                    print([time(), gps.time, gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])  # used this for testing - BL
                 #print('Writing... \n')
             #     for d in dat:
             #         f.write(str(d[0]) + ',' + str(d[1]) + ',' + str(d[2]) + ',' + str(d[3]) + ',' + str(d[4]) + ',' + str(d[5]) + '\n')
