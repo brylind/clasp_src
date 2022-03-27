6d9aa7953f3753d70e560e75b8e033b8a67cbb71
brylind <75110520+brylind@users.noreply.github.com>
2022-03-27 17:30:42 -0500

    testing gps time stuff

--- testing_misc_archived/testing_GPS.py
+++ testing_misc_archived/testing_GPS.py
@@ -47,10 +47,10 @@ def GPS():
                         #     gps.timestamp_utc.tm_hour, 
                         #     gps.timestamp_utc.tm_min,
                         #     gps.timestamp_utc.tm_sec)
-                        print([time(), gps_time, gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])  # used this for testing - BL
+                        #print([time(), gps_time, gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])  # used this for testing - BL
                         time_delta = str(datetime.datetime.utcnow()-gps_time)
-                        print(time_delta)
-                        # dat.append([time(), gps.hour, gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])
+                        #print(time_delta)
+                        dat.append([time(), time_delta, gps.hour, gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])
                     else:
                         print(([time(), 0, 0, 0, -1, -1, 0]))
                         # dat.append([time(), 0, 0, 0, -1, -1, 0])
