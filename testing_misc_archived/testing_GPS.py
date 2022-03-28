09a8b45076d93308d9b707e767b6f65fcc5c7db7
brylind <75110520+brylind@users.noreply.github.com>
2022-03-27 19:31:45 -0500

    major changes to readGPS, added delta t between gps and system clock

--- testing_misc_archived/testing_GPS.py
+++ testing_misc_archived/testing_GPS.py
@@ -10,12 +10,13 @@ def GPS():
     import socket
     import serial
     # uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=10)
-    uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=10)
+    uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=100)
 
     gps = adafruit_gps.GPS(uart, debug=False) # Use UART/pyserial
 
     # Turn on the basic GGA and RMC info (what you typically want)
-    gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+    # gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+    gps.send_command(b"PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0")
 
     # Set update rate to once a second (1hz) which is what you typically want.
     gps.send_command(b"PMTK220,10000")
