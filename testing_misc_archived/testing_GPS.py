2394f09eb61c3d658a71e37f85ecec205401361b
brylind <75110520+brylind@users.noreply.github.com>
2022-03-27 21:18:03 -0500

    major changes to readGPS, added delta t between gps and system clock

--- testing_misc_archived/testing_GPS.py
+++ testing_misc_archived/testing_GPS.py
@@ -19,7 +19,7 @@ def GPS():
     gps.send_command(b"PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0")
 
     # Set update rate to once a second (1hz) which is what you typically want.
-    gps.send_command(b"PMTK220,500")
+    gps.send_command(b"PMTK220,1500")
     device_hostname = socket.gethostname()
     launch_time = datetime.datetime.now()
     timestr = launch_time.strftime("%Y_%m_%d_%H_%M_%S")
