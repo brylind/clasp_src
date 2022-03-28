cb8321ddf445b3054bb8f7e0cd95f078fabf23d4
brylind <75110520+brylind@users.noreply.github.com>
2022-03-27 21:36:34 -0500

    changed readGPS back.. no luck

--- readGPS.py
+++ readGPS.py
@@ -9,7 +9,7 @@ def GPS():
     import busio
     import socket
     import serial
-    uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=100)
+    uart = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=10)
 
     gps = adafruit_gps.GPS(uart, debug=False) # Use UART/pyserial
     # Turn on the basic GGA and RMC info (what you typically want)
@@ -17,7 +17,7 @@ def GPS():
     gps.send_command(b"PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0")
 
     # Set update rate to once a second (1hz) which is what you typically want.
-    gps.send_command(b"PMTK220,500")
+    gps.send_command(b"PMTK220,1000")
     device_hostname = socket.gethostname()
     launch_time = datetime.datetime.now()
     timestr = launch_time.strftime("%Y_%m_%d_%H_%M_%S")
@@ -30,28 +30,28 @@ def GPS():
 
     try:
         while 1:
-            for j in range(1):      # the range(#'s) are the size of the output file (when multiplied)
+            for j in range(2):      # the range(#'s) are the size of the output file (when multiplied)
                 for i in range(60):
                     gps.update()
                     if gps.has_fix:     #gps fix: 0=no, 1=yes, 2=differential fix
-            
                         dat.append([time(), gps.latitude, gps.longitude, gps.speed_knots, gps.fix_quality, gps.satellites])
                     else:
                         dat.append([time(), 0, 0, -1, -1, 0])
                     sleep(1)
                 #print('Writing... \n')        
-            if gps.has_fix:
-                gps.update()
-                gps_time = datetime.datetime(gps.timestamp_utc.tm_year,\
-                    gps.timestamp_utc.tm_mon,\
-                    gps.timestamp_utc.tm_mday,\
-                    gps.timestamp_utc.tm_hour,\
-                    gps.timestamp_utc.tm_min,\
-                    gps.timestamp_utc.tm_sec)        
-                delta_t = (datetime.datetime.utcnow()-gps_time).total_seconds()
-            else:
-                delta_t = 'N/A'
-            f.write('Delta_t_sys_minus_gps_at_write' + ',' + str(delta_t) + '\n')
+            # if gps.has_fix:
+            #     gps.update()
+            #     gps_time = datetime.datetime(gps.timestamp_utc.tm_year,\
+            #         gps.timestamp_utc.tm_mon,\
+            #         gps.timestamp_utc.tm_mday,\
+            #         gps.timestamp_utc.tm_hour,\
+            #         gps.timestamp_utc.tm_min,\
+            #         gps.timestamp_utc.tm_sec)        
+            #     delta_t = (datetime.datetime.utcnow()-gps_time).total_seconds()
+            # else:
+            #     delta_t = 'N/A'
+            # f.write('Delta_t_sys_minus_gps_at_write' + ',' + str(delta_t) + '\n')
+
             f.write('Time_s' + ',' + 'Latitude' + ',' + 'Longitude' + ',' + 'Speed_kts' + ',' + 'GPS_fix' + ',' + 'Satellites' + '\n')
             for d in dat:
                 f.write(str(d[0]) + ',' + str(d[1]) + ',' + str(d[2]) + ',' + str(d[3]) + ',' + str(d[4]) + ',' + str(d[5]) + '\n')
