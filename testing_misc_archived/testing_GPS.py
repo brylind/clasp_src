179a8db7214fb0e52aa36d0fbea1b2da27f0d3d9
brylind <75110520+brylind@users.noreply.github.com>
2022-04-02 09:19:18 -0500

    changed gps and test gps to 100 timeout

--- testing_misc_archived/testing_GPS.py
+++ testing_misc_archived/testing_GPS.py
@@ -51,7 +51,7 @@ def GPS():
                     gps.timestamp_utc.tm_hour,\
                     gps.timestamp_utc.tm_min,\
                     gps.timestamp_utc.tm_sec)        # turns out this gets the time at the moment the gps fixed
-                gps._update_timestamp_utc(gps_time)    
+                # gps._update_timestamp_utc(gps_time)
                 delta_t = (datetime.datetime.utcnow()-gps_time).total_seconds()
                 print(f'System time: {datetime.datetime.utcnow()}')
                 print(f'GPS timestamp: {gps_time}')
