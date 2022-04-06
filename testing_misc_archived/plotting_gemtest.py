e1ce5d03d2ad5f62e5a445e02d12dea3520a17f6
brylind <75110520+brylind@users.noreply.github.com>
2022-04-06 13:58:15 -0500

    adjustments to plotting python function so I can start running syringe tests

--- testing_misc_archived/plotting_gemtest.py
+++ testing_misc_archived/plotting_gemtest.py
@@ -10,8 +10,8 @@ from scipy.fft import rfft, rfftfreq
 
 
 #df_imported = pd.read_csv("~/Documents/OneDrive - Oklahoma A and M System/Research/Infrasound/GLINDA and Brandons work/testingmic.csv")
-#df_imported = pd.read_csv("~/Documents/Github/GLINDA2_testing/testing_misc_archived/testingmic.csv")
-df_imported = pd.read_csv("~/Documents/Github/GLINDA2_testing/testing_misc_archived/SYRINGE_attaching_initial8ml.csv")
+df_imported = pd.read_csv("~/Documents/Github/GLINDA2_testing/testing_misc_archived/testingmic.csv")
+#df_imported = pd.read_csv("~/Documents/Github/GLINDA2_testing/testing_misc_archived/SYRINGE_attaching_initial8ml.csv")
 mic_gain = 107  # gain-no units
 mic_sensitivity = .000022   #V/Pa
 
