cd84e5bf9cd3d2ff4cd78e9af14676382ca41d4d
brylind <75110520+brylind@users.noreply.github.com>
2022-05-29 17:33:58 -0500

    made indexing by year, day, etc, only for mic. GPS should be indexing on remote the same it was before

--- rclone_autopush.py
+++ rclone_autopush.py
@@ -8,7 +8,7 @@
 import os
 import socket
 from time import sleep
-from datetime import date
+import datetime
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 hname = socket.gethostname()  # get hostname for indexing
 mic_data_path = f"/home/pi/glinda_main/dataFiles/mic"
@@ -17,11 +17,12 @@ sleep(20)
 
 try:
     while True:
-        year = date.today().year
-        month = date.today().month
-        day = date.today().day
+        year = datetime.datetime.now().strftime("%Y")
+        month = datetime.datetime.now().strftime("%m")
+        day = datetime.datetime.now().strftime("%d")
         mic_remote_loc = f"{hname}/{hname}_mic_data/{year}/{month}/{day}"
-        gps_remote_loc = f"{hname}/{hname}_gps_data/{year}/{month}/{day}"
+        # gps_remote_loc = f"{hname}/{hname}_gps_data/{year}/{month}/{day}"
+        gps_remote_loc = f"{hname}/{hname}_gps_data"
 
         # move files using rclone (and a minimum age filter)
         comline_mic = f'rclone move "{mic_data_path}" "glinda_data:{mic_remote_loc}" --min-age 10m'
