daf64324a8a15f3b0510dbdf8177d38e5b6e67f3
brylind <75110520+brylind@users.noreply.github.com>
2022-09-02 16:16:57 -0500

    changes to basic_readAcc.py

--- testing_misc_archived/basic_readAcc.py
+++ testing_misc_archived/basic_readAcc.py
@@ -17,9 +17,9 @@ def main():
 	while time() < t_end:
 		dat.append([time(), accel.acceleration[0], accel.acceleration[1], accel.acceleration[2]])
 	print('Done Recording \n')
-	init_SR = 1/(dat[2,1]-dat[1,1])
-	total_SR = height(dat)/(dat[-1,1]-dat[0,1])
-	print(f'Total time recording took place: {dat[-1,1]-dat[0.1]}')
+	init_SR = 1/(dat[2][1]-dat[1][1])
+	total_SR = height(dat)/(dat[-1][1]-dat[0][1])
+	print(f'Total time recording took place: {dat[-1][1]-dat[0][1]}')
 	print(f'Sample rate for initial points: {init_SR} \n')
 	print(f'Sample rate for entire file: {total_SR}')
 
