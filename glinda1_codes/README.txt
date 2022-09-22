3c808769bd0f4b52bbfcc20155bfb26ec661adc7
brylind <75110520+brylind@users.noreply.github.com>
2022-09-22 16:55:11 -0500

    removed all testing codes and added .DS_Store to .gitignore

--- glinda1_codes/README.txt
+++
@@ -1,3 +0,0 @@
-Automatic code listed in /etc/rc.local
-Automatic git push located in ~/.bashrc
-DO NOT MOVE FILES WITHOUT ADJUSTING THESE PATHS
--- glinda1_codes/references/process_delta_t.m
+++
@@ -1,9 +0,0 @@
-function [y,z] = process_delta_t(x)
-    y = x - x(1);
-    z = zeros(length(y)-1,1);
-    for i = 1:length(y)-2
-        z(i) = y(i+1)-y(i);
-    end
-    mu = mean(z)
-    sigma = std(z)
-    normplot(z);    
